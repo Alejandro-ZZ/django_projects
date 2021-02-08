23a00563d4599740b29cdac3140675c238f64c29
Alejandro Zambrano <71833624+Alejandro-ZZ@users.noreply.github.com>
2021-02-07 21:34:10 -0500

    add main view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.urls import reverse
 
 # Create your views here.
+class HomeView(View):
+    template_name = 'home/main.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
