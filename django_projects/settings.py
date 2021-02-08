ea40a8345d57ef15b7154d2b062808d982311e20
Alejandro Zambrano <71833624+Alejandro-ZZ@users.noreply.github.com>
2021-02-08 18:19:04 -0500

    forgot a coma in INSTALLED APPS

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'django.contrib.humanize'
+    'django.contrib.humanize',
     
     'home.apps.HomeConfig',
     'polls.apps.PollsConfig',
