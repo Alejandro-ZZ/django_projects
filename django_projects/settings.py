9479cc05c09ace8b773c9937ab3380095e82ef3c
Alejandro Zambrano <71833624+Alejandro-ZZ@users.noreply.github.com>
2021-02-08 18:37:50 -0500

    add crispy extension

--- django_projects/settings.py
+++ django_projects/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'django.contrib.humanize',
+    'django.contrib.humanize', # Add
+    
+    'crispy_forms', #Installed
     
     'home.apps.HomeConfig',
     'polls.apps.PollsConfig',
